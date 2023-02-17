9ab83fc616056e54121ec915c9fbbf0fb1473476
Pham Duy <phd051199@gmail.com>
2023-02-17 22:19:14 +0700

    test

--- src/config/puppeteer.ts
+++ src/config/puppeteer.ts
@@ -1,11 +1,13 @@
-import { executablePath } from "puppeteer";
+import { createBrowserFetcher } from "puppeteer";
 import puppeteer from "puppeteer-extra";
 import StealthPlugin from "puppeteer-extra-plugin-stealth";
 import { minimalArgs } from "../constants";
 
 export const initializePuppeteer = async () => {
+  const browserFetcher = createBrowserFetcher();
+  const revisionInfo = await browserFetcher.download("1083080");
   const browser = await puppeteer.launch({
-    executablePath: executablePath(),
+    executablePath: revisionInfo?.executablePath,
     headless: true,
     args: minimalArgs,
   });
