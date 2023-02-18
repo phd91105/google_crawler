87fd6194c3efaf9ad7a569d6fe14fe53612a79cb
Pham Duy <phd051199@gmail.com>
2023-02-18 11:31:09 +0700

    fix

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,6 +2,7 @@ export const cleanText = (text: string) =>
   text
     .replace(/\n\n.*$/s, '')
     .replace(/\nhttp.*$/s, '')
+    .replace(/mục\skhác.*$/is, '')
     .replace(/\n.+\d{4}/, '')
     .replace(/\d.+\d{4}/, '')
     .trim();
