51d7d26961ae91d9a41c17d16dfa323cb3bf1578
Pham Duy <phd051199@gmail.com>
2023-02-18 10:58:42 +0700

    test

--- src/config/puppeteer.ts
+++ src/config/puppeteer.ts
@@ -1,17 +1,21 @@
-import { executablePath } from 'puppeteer';
+// import { executablePath } from 'puppeteer';
 import puppeteer from 'puppeteer-extra';
 import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 
-import { minimalArgs } from '../constants';
+// import { minimalArgs } from '../constants';
 
 export const initializePuppeteer = async () => {
-  const browser = await puppeteer.launch({
-    executablePath: executablePath(),
-    headless: true,
-    args: [
-      ...minimalArgs,
-      ...(process.env.IS_LOCAL === 'true' ? [] : ['--no-sandbox']),
-    ],
+  // const browser = await puppeteer.launch({
+  //   executablePath: executablePath(),
+  //   headless: true,
+  //   args: [
+  //     ...minimalArgs,
+  //     ...(process.env.IS_LOCAL === 'true' ? [] : ['--no-sandbox']),
+  //   ],
+  // });
+
+  const browser = await puppeteer.connect({
+    browserWSEndpoint: 'ws://15.235.147.110:3000',
   });
 
   puppeteer.use(StealthPlugin());
