b44f95f965b4572fa174a2299c0870595c35d917
Pham Duy <phd051199@gmail.com>
2023-02-19 14:45:42 +0700

    rm unuse

--- src/config/puppeteer.ts
+++ src/config/puppeteer.ts
@@ -1,17 +1,13 @@
-import { executablePath } from 'puppeteer';
-import puppeteer from 'puppeteer-extra';
-import StealthPlugin from 'puppeteer-extra-plugin-stealth';
+import { executablePath,launch } from 'puppeteer';
 
 import { minimalArgs } from '../constants';
 
 export const initializeBrowser = async () => {
-  const browser = await puppeteer.launch({
+  const browser = await launch({
     executablePath: executablePath(),
     headless: true,
     args: minimalArgs,
   });
-
-  puppeteer.use(StealthPlugin());
 
   return browser;
 };
